e80fdac54392c09c54b13497a0b29b858c14c905
Denys Shapoval <url.my.email@gmail.com>
2022-02-14 15:01:23 +0200

    product-quantity oop & remove html classes up in main node

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // add to card
 
-const productsCounEl = document.getElementById("prod-count");
+const prdcsCountEl = document.getElementById("prod-count");
 const addToCartBtns = document.querySelectorAll(".add-to-cart");
 
-for (let i = 0; i < addToCartBtns.length; i++) {
-  addToCartBtns[i].addEventListener("click", () => {
-    productsCounEl.textContent =
-      +productsCounEl.textContent + +quantityInput[i].value;
+// for (let i = 0; i < addToCartBtns.length; i++) {
+//   addToCartBtns[i].addEventListener("click", () => {
+//     productsCounEl.textContent = +productsCounEl.textContent + +quantityInput[i].value;
+//     quantityInput[i].value = 1;
+//   });
+// }
+
+addToCartBtns.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    prdcsCountEl.textContent = +prdcsCountEl.textContent + +quantityInput[i].value;
     quantityInput[i].value = 1;
-  });
-}
+  })
+})
 
 // change like state
 
@@ -35,6 +41,7 @@ selectList.forEach((item) => {
 const modalW = document.querySelector(".modal");
 const moreDetailsB = document.querySelectorAll(".more-details");
 const btnClose = document.querySelector(".btn-close");
+
 function OpenClose() {
   modalW.classList.toggle("show");
 }
@@ -64,12 +71,14 @@ function startP() {
     stopEL();
   }
 }
+
 function middleP() {
   if (window.scrollY >= document.body.scrollHeight / 2) {
     OpenClose();
     stopEL();
   }
 }
+
 function endP() {
   if ($(window).scrollTop() + $(window).height() >= $(document).height() - 1) {
     OpenClose();
@@ -104,36 +113,104 @@ $(window).on("beforeunload", () => {
 //aos
 AOS.init();
 
-let decBtns = document.querySelectorAll(".decrement-button");
-let incBtns = document.querySelectorAll(".increment-button");
+// let minCount = 1;
+// let maxCount = 10;
+
+// for (let i = 0; i < quantityInput.length; i++) {
+//   let currentValue = +quantityInput[i].value;
+//   toggleButtonState(currentValue, incBtns[i], decBtns[i]);
+// }
+
+// function toggleButtonState(count, incBtn, decBtn) {
+//   decBtn.disabled = count <= minCount;
+//   incBtn.disabled = count >= maxCount;
+// }
+
+// for (let i = 0; i < decBtns.length; i++) {
+//   decBtns[i].addEventListener("click", () => {
+//     let currentValue = +quantityInput[i].value;
+//     let nextValue = currentValue - 1;
+//     quantityInput[i].value = nextValue;
+//     toggleButtonState(nextValue, incBtns[i], decBtns[i]);
+//   });
+// }
+
+// for (let i = 0; i < incBtns.length; i++) {
+//   incBtns[i].addEventListener("click", () => {
+//     let currentValue = +quantityInput[i].value;
+//     let nextValue = currentValue + 1;
+//     quantityInput[i].value = nextValue;
+//     toggleButtonState(nextValue, incBtns[i], decBtns[i]);
+//   });
+// }
+
+
+let decrementBtns = document.querySelectorAll(".decrement-button");
+let incrementBtns = document.querySelectorAll(".increment-button");
 let quantityInput = document.querySelectorAll(".product-quantity input");
-let minCount = 1;
-let maxCount = 10;
 
-for (let i = 0; i < quantityInput.length; i++) {
-  let currentValue = +quantityInput[i].value;
-  toggleButtonState(currentValue, incBtns[i], decBtns[i]);
+function Counter(
+  decrementBtn,
+  incrementBtn,
+  inputField,
+  minCount = 1,
+  maxCount = 8,
+) {
+  this.domRfs = {
+    decrementBtn,
+    incrementBtn,
+    inputField,
+
+  };
+  this.toggleButtonState = function () {
+    let count = this.domRfs.inputField.value;
+    this.domRfs.decrementBtn.disabled = count <= minCount;
+    this.domRfs.incrementBtn.disabled = count >= maxCount;
+  };
+
+  this.toggleButtonState();
+
+  this.decrement = function () {
+    this.domRfs.inputField.value = +this.domRfs.inputField.value - 1
+    this.toggleButtonState()
+  }
+
+  this.increment = function () {
+    this.domRfs.inputField.value = +this.domRfs.inputField.value + 1
+    this.toggleButtonState()
+  }
+
+  this.domRfs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  )
+
+  this.domRfs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  )
+
 }
 
-function toggleButtonState(count, incBtn, decBtn) {
-  decBtn.disabled = count <= minCount;
-  incBtn.disabled = count >= maxCount;
-}
+// let counterOneEl = new Counter(
+//   decrementBtns[0],
+//   incrementBtns[0],
+//   quantityInput[0]);
 
-for (let i = 0; i < decBtns.length; i++) {
-  decBtns[i].addEventListener("click", () => {
-    let currentValue = +quantityInput[i].value;
-    let nextValue = currentValue - 1;
-    quantityInput[i].value = nextValue;
-    toggleButtonState(nextValue, incBtns[i], decBtns[i]);
-  });
-}
+let counters = []
 
-for (let i = 0; i < incBtns.length; i++) {
-  incBtns[i].addEventListener("click", () => {
-    let currentValue = +quantityInput[i].value;
-    let nextValue = currentValue + 1;
-    quantityInput[i].value = nextValue;
-    toggleButtonState(nextValue, incBtns[i], decBtns[i]);
-  });
-}
+// for (let i = 0; i < quantityInput.length; i++) {
+//   counters[i] = new Counter(
+//     decrementBtns[i],
+//     incrementBtns[i],
+//     quantityInput[i]
+//   )
+// }
+
+quantityInput.forEach((item, i) => {
+  counters[i] = new Counter(
+    decrementBtns[i],
+    incrementBtns[i],
+    item
+  )
+});
